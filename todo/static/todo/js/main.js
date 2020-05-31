fd9b0fe7c32c1ae557b9daa513fffab4bcf2b2cd
Seth250 <ayowaleakintayo@gmail.com>
2020-05-31 13:03:13 +0100

    added more javascript functionality

--- todo/static/todo/js/main.js
+++ todo/static/todo/js/main.js
@@ -1,17 +1,29 @@
 
 function submitCheckboxForm(checkbox){
+	/* checkbox is disabled after the initial click to prevent subsequent clicks until the form
+	   is submiited */
+	checkbox.disabled = true;
 	let todo_pk = checkbox.id;
 	checkbox.form.action = `${todo_pk}/${checkbox.checked ? 'check' : 'uncheck'}/`;
 	checkbox.form.submit();
 }
 
+function disableSaveButton(){
+	let saveButton = document.getElementById('save-button');
+	// save button is disabled to prevent multiple form submissions
+	saveButton.disabled = true;
+}
+
 function initialize(){
-	elements = document.querySelectorAll("input[type='checkbox']");
+	let elements = document.querySelectorAll("input[type='checkbox']");
 	elements.forEach((checkbox) => {
 		checkbox.addEventListener('click', function(){
 			submitCheckboxForm(checkbox);
 		}, false);
 	})
+
+	let todoForm = document.getElementById('todo-form');
+	todoForm.addEventListener('submit', disableSaveButton, false);
 }
 
 window.addEventListener('load', initialize, false);
