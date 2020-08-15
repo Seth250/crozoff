8c46703528e3c491e3efa9d02cb00d0f09e8f0eb
Seth250 <ayowaleakintayo@gmail.com>
2020-08-15 17:41:08 +0100

    modified wsgi file again

--- todolist/wsgi.py
+++ todolist/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings.local')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+# application = WhiteNoise(application)
